854e260c85eaca74c1ffa9d58356858f4fa14248
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-13 21:50:29 +0630

    Add fetch user function and build case for fetching api

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,17 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchUsers = createAsyncThunk("user/fetchUsers", async () => {
+
+    const api = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const data = await api.json();
+    return data;
+ 
+});
 
 const userSlice = createSlice({
-    name: 'users',
-    initialState: {
-        users: [],
-        isLoading: true,
-        error: undefined,
-      },
-    reducers: {
+  name: "users",
+  initialState: {
+    users: ["htet", "may"],
+    isLoading: false,
+    error: undefined,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.isLoading = true;
+    });
 
-    },
-    extraReducers: {}
-})
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.users = action.payload;
+      state.error = false;
+    });
 
-export const {action, reducers} = userSlice;
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.users = [];
+      state.isLoading = false;
+      state.error = action.error.message;
+    });
+  },
+});
+
+export const { action, reducers } = userSlice;
 export default userSlice;
