cf205b7ee0f173937319311b0eaa110e276ef4fc
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-13 21:50:56 +0630

    show datas from api in the userContainer

--- src/components/UserContainer.jsx
+++ src/components/UserContainer.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from '../features/userSlice';
 
 const UserContainer = () => {
-    const {users, isLoading, error} = useSelector((state) => state.users)
+    const {users, isLoading, error} = useSelector((state) => state.users);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [users])
   return (
     <div>
-      <h2>Users - {users}</h2>
+      <h2>Users - {users.length}</h2>
       {isLoading && <div>Loading...</div>}
       {error&& <div>Error</div>}
+      <div className="user-container">
+        {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+        ))}
+      </div>
     </div>
   )
 }
